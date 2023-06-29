971d09e579e23783eacb0e9da11e68d2aa7f8e05
Elahe <1997.esm@gmail.com>
2023-06-30 00:45:58 +0330

    :zap: added logger

--- dynamic_sse/tools/data.py
+++ dynamic_sse/tools/data.py
@@ -1,10 +1,15 @@
 from typing import Tuple
+from log import get_logger
 
+logger = get_logger(__name__)
 
 class DataTools:
     
     @classmethod
     def entry_splitter(cls, entry: bytes, split_ptr: int)-> Tuple[bytes, bytes]:
+            if not entry:
+                  logger.error("entry is none")
+                  raise TypeError()
             l_hs = entry[:split_ptr]
             r_hs = entry[split_ptr:]
         
