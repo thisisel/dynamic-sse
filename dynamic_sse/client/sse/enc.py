257b8aa5d036137f95d6ba4f6f5fb1f83099dc15
Elahe <1997.esm@gmail.com>
2023-06-30 00:53:41 +0330

    :ambulance: fixed encode

--- dynamic_sse/client/sse/enc.py
+++ dynamic_sse/client/sse/enc.py
@@ -1,4 +1,3 @@
-from math import ceil
 from os import urandom
 from pathlib import Path
 from secrets import choice
@@ -7,20 +6,13 @@ from typing import Dict, List, Tuple
 from numpy import empty
 from numpy._typing import NDArray
 
-from dynamic_sse.client.structures import (
-    FilePostList,
-    LFNode,
-    LWNode,
-    NodeData,
-    WordPostList,
-)
 from dynamic_sse.client.ske import SecretKeyEnc
 from dynamic_sse.client.utils import PseudoRandomFunc
-from dynamic_sse.tools import BytesOpp, FileTools, RandOracles
+from dynamic_sse.tools import BytesOpp, FileTools, RandOracles, DataTools
 from log import get_logger
 
 # TODO give z as an argument to contractor
-FREE_LIST_INIT_SIZE = 10  # z
+FREE_LIST_INIT_SIZE = 1000  # z
 FREE = "azad"
 
 logger = get_logger(__name__)
@@ -34,31 +26,28 @@ class Encode:
         self.k3 = keys[2]
         self.k4 = keys[3]
 
-        self.search_array_size = ceil(size_c / 8) + FREE_LIST_INIT_SIZE
+        self.search_array_size = size_c+ FREE_LIST_INIT_SIZE
         self.addr_len = self.search_array_size.bit_length()
         self.ZERO = "\0" * self.addr_len
+        self.zero_bytes = self.ZERO.encode()
+        self.f_id_len = "\0" * self.k
 
         self.search_array = empty(self.search_array_size, dtype=object)
         self.dual_array = empty(self.search_array_size, dtype=object)
 
-        self.s_available_cells = [i for i in range(0, self.search_array_size)]
+        self.s_available_cells = [i for i in range(0, self.search_array_size)] #TODO reserve i=0
         self.d_available_cells = [i for i in range(0, self.search_array_size)]
 
         self.search_table = {}
         self.dual_table = {}
-        # self.ZERO = "\0" * ceil(log10(self.search_array_size))
-        self.lw_dict: Dict[str, WordPostList] = {}
-        self.lf_dict: Dict[bytes, FilePostList] = {}
         self.file_dict = {None: None}
-
-
 
     def find_usable_file_id(self):
         f_id = None
-        while f_id in self.file_ids_vault:
+        while f_id in self.file_dict.keys():
             f_id = urandom(self.k)
 
-        self.file_ids_vault.append(f_id)
+        self.file_dict.update({f_id : None})
         return f_id
 
     def find_reserve_available_cell(self, arr: type[NDArray]) -> int | None:
@@ -72,12 +61,12 @@ class Encode:
         try:
             availble_addr = choice(available_cells)
             available_cells.remove(availble_addr)
+            return availble_addr
+       
         except IndexError:
             logger.error(
                 f"search_availble cells\t{self.s_available_cells}\ndual availble cells\t{self.d_available_cells}"
             )
-
-        return availble_addr
 
     def unreserve_cell(self, arr: type[NDArray], cell_addr: int) -> None:
         arr[cell_addr] = None
@@ -97,9 +86,9 @@ class Encode:
             f_id_len=len(file_id),
         )
 
-        a = file_id + next_s_addr
-        xor_result = BytesOpp.xor_bytes(a=a, b=h1_val)
-        search_node = xor_result + ri_s
+        addrs_combo = file_id + next_s_addr
+        hashed_addrs = BytesOpp.xor_bytes(a=addrs_combo, b=h1_val)
+        search_node = hashed_addrs + ri_s
 
         return search_node
 
@@ -120,7 +109,7 @@ class Encode:
                                  k=self.k,
                                  )
 
-        a = (
+        addrs_combo = (
             next_lf_addr
             + prev_d_addr
             + next_d_addr
@@ -129,8 +118,8 @@ class Encode:
             + next_s_addr
             + f_w
         )
-        xor_result = BytesOpp.xor_bytes(a=a, b=h2_val)
-        dual_node = xor_result + ri_d
+        hashed_addrs = BytesOpp.xor_bytes(a=addrs_combo, b=h2_val)
+        dual_node = hashed_addrs + ri_d
 
         return dual_node
 
@@ -140,146 +129,31 @@ class Encode:
             self.s_available_cells
         ) and FREE_LIST_INIT_SIZE <= len(self.d_available_cells):
 
-            prev_s_free = self.ZERO.encode()
+            prev_s_free_addr = self.ZERO.encode()
+            #TODO fix phi_star == d_free_addr 
+            # prev_d_free_addr = self.ZERO.encode()
 
             for _ in range(FREE_LIST_INIT_SIZE):
                 s_free_cell = self.find_reserve_available_cell(self.search_array)
                 d_free_cell = self.find_reserve_available_cell(self.dual_array)
 
-                self.search_array[s_free_cell] = prev_s_free + d_free_cell.to_bytes(
+                self.search_array[s_free_cell] = prev_s_free_addr + d_free_cell.to_bytes(
                     self.addr_len
                 )
-                self.dual_array[d_free_cell] = self.ZERO.encode()
+                self.dual_array[d_free_cell] = urandom(6 * self.addr_len + self.k)
+                # self.dual_array[d_free_cell] = prev_d_free_addr
 
-                prev_s_free = s_free_cell.to_bytes(
+                prev_s_free_addr = s_free_cell.to_bytes(
                     self.addr_len
                 )  # last iteration = last search array free
+                # prev_d_free_addr = d_free_cell.to_bytes(self.addr_len)
 
-            self.search_table[FREE] = prev_s_free + self.ZERO.encode()
+            self.search_table[FREE] = prev_s_free_addr + self.ZERO.encode()
 
         else:
             raise IndexError(
                 f"Index error occurred while trying to insert a value under s_free_cell = {s_free_cell} or d_free_cell = {d_free_cell}"
             )
-
-    def make_arrays(self):
-        for w, w_postlist in self.lw_dict.items():
-
-            lw_n: LWNode = w_postlist.head
-            is_head = True
-
-            while lw_n is not None:
-
-                if is_head:
-                    prev_d_addr = self.ZERO.encode()
-                    prev_s_addr = self.ZERO.encode()
-
-                    is_head = False
-
-                else:
-                    prev_d_addr = lw_n.prev_d_addr_bytes(length=self.addr_len)
-                    prev_s_addr = lw_n.prev_s_addr_bytes(length=self.addr_len)
-
-                if lw_n.next_lw_node is None:
-                    next_d_addr = self.ZERO.encode()
-                    next_s_addr = self.ZERO.encode()
-
-                else:
-                    next_d_addr = lw_n.next_d_addr_bytes(length=self.addr_len)
-                    next_s_addr = lw_n.next_s_addr_bytes(length=self.addr_len)
-
-                s_node = self.make_search_node(
-                    file_id=lw_n.data.file_id,
-                    next_s_addr=next_s_addr,
-                    p_w=lw_n.data.p_w,
-                    ri_s=urandom(self.k),
-                )
-                self.search_array[lw_n.s_addr] = s_node
-
-                if (file_postlist := self.lf_dict.get(lw_n.data.file_id)) is not None:
-                    lf_n = file_postlist.find_node(w=lw_n.data.word)
-                    next_lf_addr = (
-                        lf_n.next_lf_addr_bytes(length=self.addr_len)
-                        if lf_n.next_lf_node is not None
-                        else self.ZERO.encode()
-                    )
-                else:
-                    raise KeyError(
-                        f"entry for w = {lw_n.data.word} was not created in LF under key = {lw_n.data.file_id}"
-                    )
-
-                d_node = self.make_dual_node(
-                    p_file=lw_n.data.p_file,
-                    ri_d=urandom(self.k),
-                    f_w=lw_n.data.f_w,
-                    next_lf_addr=next_lf_addr,
-                    s_addr=lw_n.s_addr.to_bytes(self.addr_len),
-                    prev_d_addr=prev_d_addr,
-                    next_d_addr=next_d_addr,
-                    prev_s_addr=prev_s_addr,
-                    next_s_addr=next_s_addr,
-                )
-                self.dual_array[lw_n.d_addr] = d_node
-
-                lw_n = lw_n.next_lw_node
-
-    def update_lf_lw(self, f_id, tokenized_words, f_file, p_file, g_file):
-
-        lf = FilePostList()
-        is_lf_head = True
-
-        for w in tokenized_words:
-
-            is_lw_head = False
-
-            with PseudoRandomFunc(
-                k1=self.k1, k2=self.k2, k3=self.k3, k=self.k
-            ) as w_prf:
-
-                f_w, g_w, p_w = w_prf.get_word_hashes(word=w, length=2 * self.addr_len)
-
-            s_addr = self.find_reserve_available_cell(arr=self.search_array)
-            d_addr = self.find_reserve_available_cell(arr=self.dual_array)
-            if not (s_addr and d_addr):
-                logger.error(
-                    f"Could not make update LF and LW for {f_id}), d_addr, s_addr were not resolved"
-                )
-                raise IndexError
-
-            n_data = NodeData(
-                word=w,
-                file_id=f_id,
-                f_w=f_w,
-                p_w=p_w,
-                g_w=g_w,
-                p_file=p_file,
-                f_file=f_file,
-                g_file=g_file,
-            )
-            lw_node = LWNode(data=n_data, s_addr=s_addr, d_addr=d_addr)
-            lf_node = LFNode(data=n_data, s_addr=s_addr, d_addr=d_addr)
-
-            if (lw := self.lw_dict.get(w)) is None:
-                is_lw_head = True
-                lw = WordPostList()
-                logger.debug(f" New word detected : {w}")
-
-            lw.insert_at_tail(lw_node)
-            lf.insert_at_tail(lf_node)
-
-            if is_lw_head:
-                a = s_addr.to_bytes(self.addr_len) + d_addr.to_bytes(self.addr_len)
-                self.search_table[f_w] = BytesOpp.xor_bytes(a=a, b=g_w)
-
-            if is_lf_head:
-                self.dual_table[f_file] = BytesOpp.xor_bytes(
-                    a=d_addr.to_bytes(length=self.addr_len), b=g_file
-                )
-                is_lf_head = False
-
-            self.lw_dict[w] = lw
-
-        self.lf_dict[f_id] = lf
 
     def make_lf_lw(self, f_id: bytes, tokenized_words: List[str], f_file: bytes, p_file: bytes, g_file: bytes):
         next_lf_addr = self.zero_bytes
@@ -322,15 +196,16 @@ class Encode:
             self.dual_array[d_cell] = dual_node
 
             if next_d_addr != self.zero_bytes:
-                self.shift_d_node_forward(d_addr=next_d_addr, p_file=p_file, prev_s_addr=s_addr, prev_d_addr=d_addr)
+                self.update_d_node_prev_addrs(d_addr=next_d_addr, p_file=p_file, prev_s_addr=s_addr, prev_d_addr=d_addr)
            
             self.search_table[f_w] = BytesOpp.xor_bytes(s_addr+d_addr, g_w)
 
             next_lf_addr = d_addr
 
         self.dual_table[f_file] = BytesOpp.xor_bytes(d_addr, g_file)
+        logger.debug(f'New File Encoded f_id = {f_id}')
 
-    def shift_d_node_forward(self, d_addr: bytes, p_file: bytes, prev_s_addr: bytes, prev_d_addr: bytes):
+    def update_d_node_prev_addrs(self, d_addr: bytes, p_file: bytes, prev_s_addr: bytes, prev_d_addr: bytes):
         d_node = self.dual_array[int.from_bytes(d_addr)]
         d_entry_hashed, rd = DataTools.entry_splitter(entry=d_node, split_ptr=6*self.addr_len+self.k)
         
@@ -340,14 +215,14 @@ class Encode:
         old_prev_d_addr = d_entry[self.addr_len : 2 * self.addr_len]
         old_prev_s_addr = d_entry[4 * self.addr_len : 5 * self.addr_len]
 
-        cleaner_str = self.zero_bytes + old_prev_d_addr +2 * self.zero_bytes + old_prev_s_addr + self.zero_bytes + ("\0" * self.k).encode()
+        wiper_str = self.zero_bytes + old_prev_d_addr +2 * self.zero_bytes + old_prev_s_addr + self.zero_bytes + ("\0" * self.k).encode()
         plug_str = self.zero_bytes + prev_d_addr +2 * self.zero_bytes + prev_s_addr + self.zero_bytes + ("\0" * self.k).encode()
 
-        cleaned_entry = BytesOpp.xor_bytes(d_entry, cleaner_str)
-        shifted_entry = BytesOpp.xor_bytes(cleaned_entry, plug_str)
+        pluggable_entry = BytesOpp.xor_bytes(d_entry, wiper_str)
+        updated_entry = BytesOpp.xor_bytes(pluggable_entry, plug_str)
 
-        shifted_entry_hashed = BytesOpp.xor_bytes(shifted_entry, h2_val)
-        self.dual_array[int.from_bytes(d_addr)] = shifted_entry_hashed + rd
+        updated_hashed_entry = BytesOpp.xor_bytes(updated_entry, h2_val)
+        self.dual_array[int.from_bytes(d_addr)] = updated_hashed_entry + rd
 
     def enc(self, raw_files_dir: str, encoded_dir: str, ske: SecretKeyEnc):
         raw_files_dir_path = Path(raw_files_dir)
@@ -371,12 +246,12 @@ class Encode:
                     )
                     continue
 
-                f_id = self._find_usable_file_id()
+                f_id = self.find_usable_file_id()
                 tokenized_words = FileTools.tokenize_txt_file(entry)
 
                 try:
 
-                    self.update_lf_lw(
+                    self.make_lf_lw(
                         f_id=f_id,
                         tokenized_words=tokenized_words,
                         f_file=f_file,
@@ -387,16 +262,17 @@ class Encode:
                     ske.enc_file(
                         in_file=entry, out_file=f"{encoded_dir}/file_{file_num}.bin"
                     )
+                    #TODO insert c in db along with f_id as pk
 
                     logger.debug(
                         f"[{file_num}] - File {entry} was encoded under {encoded_dir}/file_{file_num}.bin"
                     )
 
-                    self.file_dict.update({f_id: entry})
+                    # self.file_dict.update({f_id: entry})
+                    self.file_dict.update({f_id: f'{encoded_dir}/file_{file_num}.bin'})
                     file_num += 1
 
                 except IndexError:
-                    # self.file_ids_vault.remove(f_id)
                     self.file_dict.pop(f_id)
                     logger.error(
                         f"FILE {entry} was IGNORED.\n-->Because of a failure in updating LF, LW.\n++Released file_id : {f_id} "
@@ -404,5 +280,6 @@ class Encode:
                     continue
 
         self.make_free_lists()
-        self.make_arrays()
+        
+        return self.search_array, self.search_table, self.dual_array, self.dual_table
 
