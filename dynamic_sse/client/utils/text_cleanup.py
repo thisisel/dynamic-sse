18f4c28231ecba8b36adc4cb11467d432b56d370
Elahe <1997.esm@gmail.com>
2023-07-02 10:48:06 +0330

    :fire: removed redundant file: text_cleanup.py

--- dynamic_sse/client/utils/text_cleanup.py
+++
@@ -1,9 +0,0 @@
-import re
-from typing import List
-
-def tokenize_text(corpus : str) -> List[str]:
-    clean_text = re.sub(r"[^A-Za-z0-9\s]+", "", corpus.lower())
-    
-    tokens = [t for t in clean_text.split()]
-    
-    return tokens
