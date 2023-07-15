95af92c725fccab68b0d1ca9495277f3fa1be019
Elahe <1997.esm@gmail.com>
2023-07-15 12:48:19 +0330

    :pencil2: fixed test for key generation

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -4,43 +4,20 @@ from numpy.random import default_rng
 
 from dynamic_sse.client.sse.gen import Generate
 
-
-def test_key_generation_value_error():
-
-    with pytest.raises(ValueError) as err:
-        _ = Generate._gen_keys(k=0, bit_generator=default_rng())
-
-
-# security_param: int = randint(1, 90)
 security_param: int = choice((20, 28, 32, 64))
 
 
-@pytest.mark.parametrize("key_num , is_triple", [(3, True), (1, False)])
-def test_key_generation(key_num: int, is_triple: bool):
+def test_key_generation():
 
-    keys = Generate._gen_keys(
-        k=security_param, bit_generator=default_rng(), triple_keys=is_triple
-    )
+    keys = Generate.get_keys(k=security_param)
 
-    assert type(keys) is list
-    assert len(keys) == key_num
+    assert len(keys) == 4
 
-    for k in keys:
-        assert type(k) is bytes
-        assert len(k) == security_param
-        assert all(k) is "0" or "1"
-
-
-def test_get_keys_from_os_pool():
-    keys = Generate.get_keys(k=security_param, os_pool=True)
-
-    assert type(keys) == tuple
-    assert len((keys)) == 4
-
-    for k in keys:
-        assert type(k) is bytes
-        assert len(k) == security_param
-
+    for i in range(3):
+        assert type(keys[i]) is bytes
+        assert len(keys[i]) == security_param
+    assert len(keys[3]) == 1
+    assert len(keys[3][0]) == 44
 
 def test_get_keys_malformed_k():
     with pytest.raises(ValueError) as err:
