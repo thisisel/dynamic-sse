17561693a914cb7873deb93a9cedd82cc2201199
Elahe <1997.esm@gmail.com>
2023-07-27 23:49:54 +0330

    fixed typos in gen.py

--- dynamic_sse/client/sse/gen.py
+++ dynamic_sse/client/sse/gen.py
@@ -16,11 +16,10 @@ class Generate:
 
         if k_4_num < 1:
             raise ValueError(
-                f"number of requested k4 keys must be higher than or equal to 1"
+                f"number of requested k4 keys must be more than or equal to 1"
             )
 
         triple_keys = [os.urandom(k) for _ in range(3)]
-        # k_4 = Fernet.generate_key()
         k_4 = [Fernet.generate_key() for _ in range(k_4_num)]
 
         return (*triple_keys, k_4)
