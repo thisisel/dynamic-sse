30d8283d2db171c34a687363970a0f83f09059c6
Elahe <1997.esm@gmail.com>
2023-07-27 23:39:20 +0330

    moved config.py to client

--- dynamic_sse/client/config.py
+++ dynamic_sse/client/config.py
@@ -1,5 +1,4 @@
 import os
-from typing import Optional
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,3 +7,4 @@ FREE = "azad"
 
 PLAIN_DIR = os.getenv('PLAIN_DIR') or r''
 ENCODED_DIR = os.getenv('ENCODED_DIR') or r''
+KEYS_PATH = os.getenv("KEYS_PATH")
