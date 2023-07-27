f53563cb7d012ab382d786b8772722fef87ee0f4
Elahe <1997.esm@gmail.com>
2023-07-27 23:50:54 +0330

    started implementing the facade for client

--- dynamic_sse/client/core.py
+++ dynamic_sse/client/core.py
@@ -1,18 +1,31 @@
 from dynamic_sse.client.ske import SecretKeyEnc
 from dynamic_sse.client.sse import Encode, Generate
+from dynamic_sse.client.config import PLAIN_DIR
 from dynamic_sse.tools import FileTools
-from dynamic_sse.config import PLAIN_DIR
 
 class Client:
-    def __init__(self, security_param: int = 32) -> None:
+    def __init__(self, security_param: int = 32, key_ring_reset : bool = True) -> None:
         self.security_param = security_param
-        # TODO try to import keys from a file
-        self.keys = Generate.get_keys(
-                k=self.security_param,
-            )
+
+        if key_ring_reset:
+            self.keys = Generate.get_keys(
+                    k=self.security_param,
+                )
         
         self.ske = SecretKeyEnc(fernet_keys=self.keys[3])
         _, size_c = FileTools.get_dir_files_stats(PLAIN_DIR)
-        self.encode_obj = Encode(size_c=size_c, k= self.security_param, keys=self.keys)
+        self.encode_obj = Encode(size_c=size_c, keys=self.keys)
+
+    def upload(self):
+        pass
+
+    def search(self):
+        pass
+
+    def add(self):
+        pass
+
+    def delete(self):
+        pass
     
 
