7cdd6c00fe9c0c9064295ea4f4f402b4cab35b37
Elahe <1997.esm@gmail.com>
2023-07-31 00:24:28 +0330

    updated client config

--- dynamic_sse/client/config.py
+++ dynamic_sse/client/config.py
@@ -5,6 +5,8 @@ load_dotenv()
 
 FREE = "azad"
 
-PLAIN_DIR = os.getenv('PLAIN_DIR') or r''
-ENCODED_DIR = os.getenv('ENCODED_DIR') or r''
-KEYS_PATH = os.getenv("KEYS_PATH")
+PLAIN_DIR = os.getenv('PLAIN_DIR') 
+ENC_DIR = os.getenv('ENCODED_DIR')
+TRIPLE_KEYS_PATH = os.getenv("TRIPLE_KEYS_PATH")
+FOURTH_KEYS_PATH = os.getenv("FOURTH_KEYS_PATH")
+ENC_FILE_PATHS_DB = os.getenv("ENC_FILE_PATHS_DB") or "encrypted_files_db"
