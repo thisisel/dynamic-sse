614b3e08e927212ba88ea280bdcd3e0c96dc64a3
Elahe <1997.esm@gmail.com>
2023-07-31 11:44:48 +0330

    updated test_ske

--- tests/test_ske.py
+++ tests/test_ske.py
@@ -1,10 +1,12 @@
 from pathlib import Path
-from typing import Dict, List
+from typing import List
+
 import pytest
 from cryptography.fernet import Fernet
-from dynamic_sse.client.ske import SecretKeyEnc
-from .conftest import TestDataPaths
 
+from dynamic_sse.client.ske import SecretKeyEnc
+
+from .conftest import TestDataPaths
 
 
 @pytest.fixture(scope="module")
@@ -13,36 +15,19 @@ def test_single_key() -> List[bytes]:
 
 
 @pytest.fixture(scope="module")
-def test_files():
-
-    return {
-        "plain_file": r"/home/elahe/Projects/Python/dynamic_sse/tests/test_data/plain/four.txt",
-        "enc_file": r"/home/elahe/Projects/Python/dynamic_sse/tests/test_data/encrypted/four_enc.bin",
-        "dec_file": r"/home/elahe/Projects/Python/dynamic_sse/tests/test_data/decrypted/four_dec.txt",
-    }
-
-
-@pytest.fixture(scope="module")
 def test_ske(test_single_key) -> SecretKeyEnc:
     return SecretKeyEnc(fernet_keys=test_single_key)
 
 
-def test_enc_single_file(test_ske: SecretKeyEnc, test_files: Dict[str, str]):
-
-    test_ske.enc_file(in_file=test_files["plain_file"], out_file=test_files["enc_file"])
-
-
-def test_dec_single_file(test_ske: SecretKeyEnc):
-    plain_file = f'{TestDataPaths.PLAIN_DIR.value}/1.txt'
-    enc_file = f'{TestDataPaths.ENC_DIR.value}/1_enc.bin'
-    dec_file = f'{TestDataPaths.DEC_DIR.value}/1_dec.txt'
+def test_enc_dec_single_file(test_ske: SecretKeyEnc):
+    plain_file = f"{TestDataPaths.PLAIN_DIR.value}/1.txt"
+    enc_file = f"{TestDataPaths.ENC_DIR.value}/1_enc.bin"
+    dec_file = f"{TestDataPaths.DEC_DIR.value}/1_dec.txt"
 
     test_ske.enc_file(in_file=plain_file, out_file=enc_file)
     test_ske.dec_file(in_file=enc_file, out_file=dec_file)
 
-    with open(plain_file, "r") as p_f, open(
-        dec_file, "r"
-    ) as dec_f:
+    with open(plain_file, "r") as p_f, open(dec_file, "r") as dec_f:
         plain_txt = p_f.read()
         decoded_txt = dec_f.read()
 
@@ -50,7 +35,6 @@ def test_dec_single_file(test_ske: SecretKeyEnc):
 
     Path(enc_file).unlink(missing_ok=True)
     Path(dec_file).unlink(missing_ok=True)
-
 
 
 def test_enc_dir_file(test_ske: SecretKeyEnc):
